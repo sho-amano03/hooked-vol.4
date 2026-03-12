c5ccea75bc01a726001f6e00d1ade2675110c720
Sho Amano <shou1200@icloud.com>
2026-03-13 08:06:19 +0900

    reset repo without heavy files

---
+++ script.js
@@ -0,0 +1,51 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu-links");
+// 開閉
+hamburger.addEventListener("click",() =>{
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+// メニューのリンク押したら閉じる
+menu.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
+
+document.addEventListener("click", (e) => {
+    const clickedHamburger = hamburger.contains(e.target);
+    const clickedMenu = menu.contains(e.target);
+
+    if(!clickedHamburger && !clickedMenu){
+        menu.classList.remove("active");
+    }
+});
+
+const targets = document.querySelectorAll(".reveal");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-show");
+      observer.unobserve(entry.target); 　//  一回だけ
+    }
+  });
+},{
+  threshold:0.25　 // ~%見えたら発火
+});
+targets.forEach((target) => observer.observe(target));
+
+// PAST PHOTO
+
+const eventBoxes = document.querySelectorAll(`.event-box`);
+
+eventBoxes.forEach((box) => {
+  box.addEventListener(`toggle`, () => {
+    if (box.open){
+      eventBoxes.forEach((otherBox) => {
+        if (otherBox !== box){
+          otherBox.open = false;
+        }
+      });
+    }
+  });
+});
